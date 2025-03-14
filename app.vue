5c15e94d2a8163224157821b4b4f0a23b18b8703
fningcom <54799370+fningcom@users.noreply.github.com>
2025-03-14 10:43:23 +0200

    главная для xminer

--- app.vue
+++ app.vue
@@ -2,7 +2,22 @@
   <div>
     <NuxtRouteAnnouncer />
     <div class="container">
-      <h1>НОвая главная страница</h1>
+      <img src="/img/logo.webp" alt="">
     </div>
   </div>
 </template>
+
+<style>
+body {
+  /*background: linear-gradient(to right, #1e1f27, #4a8998);*/
+  background: linear-gradient(to right, #202126, #263940, #1f5542);
+}
+img {
+  width: 550px;
+  margin-top: 80px;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.5);
+}
+.container {
+  text-align: center;
+}
+</style>
